951e3f2146305ccdafb1b21259fc167183400449
claudioverdolini <claudiov.cobe@gmail.com>
2026-09-09 15:28:28 +0200

    Load cloud auth and sync styles

--- main.tsx
+++ main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './app.css'
 import './responsive.css'
+import './cloud.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
